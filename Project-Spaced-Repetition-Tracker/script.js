e432972cf101bdb9f1c64f03e896fddfcc2863f2
saqib javed <70890519+saqibjvd@users.noreply.github.com>
2025-02-05 13:48:25 +0000

    Update script.js

--- Project-Spaced-Repetition-Tracker/script.js
+++ Project-Spaced-Repetition-Tracker/script.js
@@ -17,22 +17,21 @@ window.onload = function () {
    option.value = userId;
    option.textContent = `user ${userId}`
   userSelect.append(option)
-  })
-}
+  });
+};
 
 
-
-// getUserIds() .. done
+// getUserIds() .. saqib done
 //when called, returns an array of strings, each of which is a user id
 
-// getData(userId)
+// getData(userId) .. donara
 //when called with a user id string as an argument, returns an array of objects, 
 //each of which represents an agenda item for the user
 
-// addData(userId, data)
+// addData(userId, data) ...saqib
 //when called with a user id string and an array of objects as arguments, it will append the agenda items data to the user’s stored agenda. Each of the objects should contain information about the agenda item, such as the date and topic that should be revised on that date. The function does not return anything
 
-//clearData(userId)
+//clearData(userId)... donara
 // when called with a user id string as an argument, 
 //it will clear any stored data associated with the user id. 
 //This is provided to help with development, and is not required in the final code
